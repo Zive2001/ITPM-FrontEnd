e5e642c78c978f552005406044e78d1f02f137dd
Zive2001 <supunseneviratne@gmail.com>
2024-03-06 11:09:59 +0530

    error fixed

--- login/src/App.jsx
+++ login/src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import SignIn from "./Pages/SignIn"
+import SignUp from "./Pages/SignUp"
 
 export default function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/sign-in" element ={<SignIn/>} />
-      <Route path="/sign-up" element ={<SignUp/>} />
-    
+      <Route path ="/sign-up" element ={<SignUp/>}/>
     </Routes>
       
     </BrowserRouter>
